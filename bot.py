4adc84bc8ab4424a75143f1184853101e7a6d436
Penyqiu <59291319+Penyqiu@users.noreply.github.com>
2020-04-05 13:35:44 +0200

    Add files via upload
    Colors to gray to better recognize lines

--- bot.py
+++ bot.py
@@ -1,14 +1,19 @@
 import numpy as np
 import PIL
 import cv2
+import time
 from PIL import ImageGrab
 
-while(True):
-    printscreen_pil = ImageGrab.grab()
-    printscreen_numpy = np.array(printscreen_pil.getdata(),dtype='uint8')\
-    .reshape((printscreen_pil.size[1],printscreen_pil.size[0],3))
+def process_img(original_image):
 
-    cv2.imshow('window',printscreen_numpy)
+    processed_img = cv2.cvtColor(original_image,cv2.COLOR_BGR2GRAY)
+
+last_time = time.time()
+while(True):
+    screen = np.array(ImageGrab.grab(bbox=(0,40,800,640)))
+    print ('Klatki {}'.format(time.time()-last_time))
+    last_time= time.time()
+    cv2.imshow('window',cv2.cvtColor(screen,cv2.COLOR_BGR2RGB))
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
